20aa80408095dc28007ea4675be1c41a32bc3102
YangJiLei <2031291160@qq.com>
2025-09-14 16:08:39 +0800

    Account加锁

--- Account.cpp
+++ Account.cpp
@@ -4,15 +4,23 @@ Account::Account(int id, int balance) : id(id), balance(balance) {}
 
 void Account::deposit(int amount)
 {
+    std::lock_guard<std::mutex> lock(mtx);
     balance += amount;
 }
 
 bool Account::withdraw(int amount)
 {
-    balance -= amount;
+    std::lock_guard<std::mutex> lock(mtx);
+    if (balance >= amount)
+    {
+        balance -= amount;
+        return true;
+    }
+    return false;
 }
 
 int Account::getBalance()
 {
+    std::lock_guard<std::mutex> lock(mtx);
     return balance;
 }
